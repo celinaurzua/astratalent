e389f41010464c13c800f78cd264a29d49e34ab6
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-12-06 00:46:20 -0600

    Text is added to component TestFeedback and TestBye

--- src/Components/Testbye.js
+++ src/Components/Testbye.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class Testbye extends Component {
   render() {
     return (
       <div>
-        <Header/>
-          <p>Gracias!!! </p>
-          <Link to="/">
+        <Header />
+        <p>Congratulations! You have finished this test section.</p>{" "}
+        <p>Wait for communication for the next part of the process.</p>
+        <Link to="/">
           <button>Log out</button>
-          </Link>
-          <Footer/>
+        </Link>
+        <Footer />
       </div>
     );
   }
--- src/Components/Testfeedback.js
+++ src/Components/Testfeedback.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
-import menuFeedback from '../Images/menu-cuatro.png';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import menuFeedback from "../Images/menu-cuatro.png";
 
 class Testfeedback extends Component {
-
   render() {
     return (
       <div>
@@ -15,12 +14,36 @@ class Testfeedback extends Component {
           <img src={menuFeedback} alt="" />
 
           <div>
-            <p></p>
-
-            <Link to="testbye">
-              <button>SEND FEEDBACK</button>
-            </Link>
+            <p>
+              1. Do you think that the content of the tests correspond to the
+              position you are applying for?
+            </p>
+            <div>
+              <input value="Totally agree" type="radio" name="feedback" />
+              <label>Totally agree</label>
+            </div>
+            <div>
+              <input value="So so" type="radio" name="feedback" />
+              <label>So so</label>
+            </div>
+            <div>
+              <input value="Totally disagree" type="radio" name="feedback" />
+              <label>Totally disagree</label>
+            </div>
           </div>
+          <p>
+            2. Is there anything you would like to highlight or improve in this
+            process?
+          </p>
+          <div>
+            <input
+              style={{ width: "30rem", height: "15rem" }}
+              type="textarea"
+            />
+          </div>
+          <Link to="testbye">
+            <button>SEND FEEDBACK</button>
+          </Link>
         </div>
         <Footer />
       </div>
