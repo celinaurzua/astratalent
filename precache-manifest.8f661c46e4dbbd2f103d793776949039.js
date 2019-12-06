b5c5532e92f089c864f72873ce884257b2799030
celinaurzua <celina.urzua@gmail.com>
2019-12-06 12:57:14 -0600

    Updates

---
+++ precache-manifest.8f661c46e4dbbd2f103d793776949039.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd484f0ed2f82f2c723eb8b72ec8a6d",
+    "url": "/astratalent1/index.html"
+  },
+  {
+    "revision": "1aab74097c99c74399a5",
+    "url": "/astratalent1/static/css/main.0f2a5e4b.chunk.css"
+  },
+  {
+    "revision": "95ff76815201e185bdcf",
+    "url": "/astratalent1/static/js/2.8773f9e1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/astratalent1/static/js/2.8773f9e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1aab74097c99c74399a5",
+    "url": "/astratalent1/static/js/main.87c6c996.chunk.js"
+  },
+  {
+    "revision": "8abb042fc2bbcf55d2d7",
+    "url": "/astratalent1/static/js/runtime-main.114dd5fd.js"
+  },
+  {
+    "revision": "40d0e213b47216a951b631ded4be2d11",
+    "url": "/astratalent1/static/media/login-ilustration.40d0e213.png"
+  }
+]);
