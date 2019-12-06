362feb45567d895d02b33031f7ae443fbef13919
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-12-05 23:59:17 -0600

    eh???

--- src/Components/Testone.js
+++ src/Components/Testone.js
@@ -1,4 +1,4 @@
-import React, { Component, Checkbox } from "react";
+import React, { Component} from "react";
 import { Link } from "react-router-dom";
 import Timer from "../Components/Timer/Timer";
 import Header from "./Header";
@@ -22,7 +22,7 @@ class Testone extends Component {
       visible: true
     });
   };
-  
+
   render() {
     console.log(Data.data)
     return (
