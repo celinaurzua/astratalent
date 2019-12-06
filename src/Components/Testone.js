e710c09f936f56257f8d6d9efcd4c76d82018f93
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-12-05 22:08:23 -0600

    Se pintan preguntas y respuestas con radio bottom

--- src/Components/Testone.js
+++ src/Components/Testone.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Timer from '../Components/Timer/Timer'
-import Header from './Header';
-import Footer from './Footer';
-import menuTestOne from '../Images/menu-uno.png';
-
-
+import React, { Component, Checkbox } from "react";
+import { Link } from "react-router-dom";
+import Timer from "../Components/Timer/Timer";
+import Header from "./Header";
+import Footer from "./Footer";
+import menuTestOne from "../Images/menu-uno.png";
+import Data from "./Data";
 
 class Testone extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        visible:false,
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false
+    };
+  }
 
-  AlertStartTest=()=> {
-    alert("You can answer this test only once. If you decide to leave, the test will be blocked and you will not be able to continue answering questions. You can answer this test only once. Are you sure you want to leave the test now?")
+  AlertStartTest = () => {
+    alert(
+      "You can answer this test only once. If you decide to leave, the test will be blocked and you will not be able to continue answering questions. You can answer this test only once. Are you sure you want to leave the test now?"
+    );
     this.setState({
-      visible:true,
-    })
-   }
-   render() {
+      visible: true
+    });
+  };
+  render() {
+    console.log(Data.data)
     return (
       <div>
         <Header />
@@ -31,15 +33,32 @@ class Testone extends Component {
 
           <div>
             <p></p>
-<button onClick={() => this.AlertStartTest()}>Iniciar Test</button>
-<Timer visible={this.state.visible} />
+            <button onClick={() => this.AlertStartTest()}>Iniciar Test</button>
+            <Timer visible={this.state.visible} />
+            {Data.data && Data.data.map(element => 
+              element.logicalTest.map((el, id) => 
+             <div>
+             <p key={id}>{el.question} </p>
+             <div>
+             <input key={id} value={el.a[1]} type= "radio" name={el.question[0]}/>
+             <label key={id}>{el.b[0]}</label>
+             </div>
+             <div>
+             <input key={id} value={el.b[1]} type= "radio" name={el.question[0]}/>
+             <label key={id}>{el.c[0]}</label>
+             </div>
+             <div>
+             <input key={id} value={el.c[1]} type= "radio" name={el.question[0]}/>
+             <label key={id}>{el.d[0]}</label>
+             </div>
+             </div>
+             ))}
             <Link to="testtwo">
               <button>START TEST 2</button>
             </Link>
           </div>
         </div>
         <Footer />
-
       </div>
     );
   }
